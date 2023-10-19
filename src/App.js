d34e3b69c497759b9c986d9cd287ecfb3e49a548
nehaleloraby <nehal.eloraby17@gmail.com>
2023-10-19 14:40:52 -0400

    Debugging

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
-import { Routes, Route, Navigate } from 'react-router-dom'
+import { Routes, Route, } from 'react-router-dom'
 import './App.css'
 import NavBar from './components/NavBar'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import SignUp from './pages/SignUp'
-import Account from './pages/Account'
+
 
 function App() {
-  // Checking if the user is signed in and grab user ID from localStorage
-  const isUserSignedIn = !!localStorage.getItem('token')
-  const userId = localStorage.getItem('userId')
-
+ 
   return (
     <div className="App">
       <NavBar />
